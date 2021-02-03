d4f1873c742e85f79ed8f19da6156fc365292b9b
LUMOS INC <LUMOS INC@LUMOS>
2021-02-03 14:48:45 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustemerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustemerManager
+    {
+        public void add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + customer.Surname + "DataBase'e eklendi.");
+        
+        }
+
+
+        public void list(Customer customer)
+        {
+
+            Console.WriteLine(customer.Id + "  Id numaralı müşteri listelendi.");
+        }
+
+        public void update (Customer customer)
+        {
+
+            Console.WriteLine("veritabanımıza 1 yeni müşteri eklendi.");
+        }
+
+       public void info (Customer customer)
+        {
+            Console.WriteLine(customer.Id +"\n"+ customer.Name +"\n"+ customer.Surname+"\n"+customer.TCId+"\n"+customer.City+"\n");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 801;
+            customer1.Name = "serdar";
+            customer1.Surname = "ISIK";
+            customer1.TCId = "20455626730";
+            customer1.City = "BURSA";
+
+
+            CustemerManager custemerManager = new CustemerManager();
+            custemerManager.add(customer1);
+            custemerManager.list(customer1);
+            custemerManager.update(customer1);
+
+            Console.WriteLine();
+
+            custemerManager.info(customer1);
+
+        }
+    }
+}
